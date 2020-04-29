9fcb605ff7ecf93fdeb5d6d1350a35d4e20c98bb
Cristian Padilla <Cristancho2011@gmail.com>
2020-04-29 11:13:01 -0500

    Se agrego el hosting y el login con facebook

---
+++ app.js
@@ -0,0 +1,38 @@
+var providerGoogle = new firebase.auth.GoogleAuthProvider();
+var providerFacebook = new firebase.auth.FacebookAuthProvider();
+//jquery 
+$('#loginGoogle').click(function(){
+    firebase.auth()
+    .signInWithPopup(providerGoogle)
+    .then(function(result){
+        console.log(result.user);
+        $('#loginGoogle').hide();
+        $('#root').append(result.user.displayName);
+        guardarDatos(result.user);
+        // $('#root').append("<img src='"+result.user.photoURL+"'/>" );
+        $('avatar').attr('src',result.user.photoURL)
+    });
+});
+
+$('#loginFacebook').click(function(){
+    firebase.auth()
+    .signInWithPopup(providerFacebook)
+    .then(function(result){
+        console.log(result.user);
+        $('#loginFacebook').hide();
+        $('#root').append(result.user.displayName);
+        guardarDatos(result.user);
+        //$('#root').append("<img src='"+result.user.photoURL+"'/>" );
+        $('avatar').attr('src',result.user.photoURL)
+    });
+});
+
+function guardarDatos(user){
+    var usuario ={
+        uid : user.uid,
+        nombre : user.displayName,
+        email : user.email,
+        foto : user.photoURL,
+    }
+    firebase.database().ref("TALLER05/Usuarios/"+user.uid).set(usuario)
+}
